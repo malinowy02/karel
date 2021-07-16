f3360997a3cdf395a1fdb26f25c4a7adbb1fc2f2
Hubert Malinowski <hubert.malina@gmail.com>
2021-07-16 13:58:59 +0200

    Skladanie stringa w py

--- DZIALAJACE/TMP_Serwer.py
+++ DZIALAJACE/TMP_Serwer.py
@@ -33,6 +33,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn.send(out_byt)  #wiadomosc wysylana do Karela
             if out_str[:1] == '0':        #gdy wpiszesz 0 zamknij serwer
                 break
+            
+            if out_str[:1] == 'p':
+                out_str=input("Enter X,Y,Z,W,P,R: ")
+                temp=out_str.split(',')
+                out_str=''
+                for i in range(len(temp)):
+                    g=float("{:.3f}".format(float(temp[i])))
+                    #print('liczba=',g)
+                    s=str(g)
+                    out_str=out_str+s+(9-len(s))*' '
+                out_str=out_str + (126-len(out_str))*' '    #formatowanie wiadomosci do dlugosci jakiej oczekuje karel
+                out_byt=bytes(out_str,'utf-8')  #zakodowanie wiadomosci na bity
+                conn.send(out_byt)  #wiadomosc wysylana do Karela
 
             message = str(data) + '\n'  #formatowanie do pliku z nastepna linia
             f.write(message)
--- DZIALAJACE/Testing.py
+++ DZIALAJACE/Testing.py
@@ -5,6 +5,14 @@ import timeit
 
 
 
-out_str='02456666     '
-str2=out_str[:2]
-print('wyjscie: ',str2)
+
+out_str=input("Enter X,Y,Z,W,P,R: ")
+temp=out_str.split(',')
+print(temp)
+out_str=''
+for i in range(len(temp)):
+    g=float("{:.3f}".format(float(temp[i])))
+    #print('liczba=',g)
+    s=str(g)
+    out_str=out_str+s+(9-len(s))*' '
+print(out_str)
