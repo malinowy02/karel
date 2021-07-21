b99f3f11ea7d86e219797024f34f06916c30df5d
Hubert Malinowski <hubert.malina@gmail.com>
2021-07-21 09:29:45 +0200

    Dziala zabezpieczenie przed literami

--- DZIALAJACE/TMP_Serwer.py
+++ DZIALAJACE/TMP_Serwer.py
@@ -11,6 +11,9 @@ class LimitsException(Exception):
 class QuitException(Exception):
     pass
 
+class BadFormatException(Exception):
+    pass
+
 INPUT_KEYS = ["X_in", "Y_in", "Z_in", "W_in", "P_in", "R_in"]
 AXES_LIMITS = [(-900, 900), (-900, 900), (-900, 900), (-180, 180), (-180, 180), (-180, 180)]
 PR_LIMITS = (1,100)
@@ -18,29 +21,33 @@ PR_LIMITS = (1,100)
 
 # MAIN FUNCTIONALITIES
 def writepos (conn, **kwargs):
-    
     # getting from kwargs
     values = kwargs.get("values", [0,0,0, 0, 0,0])
     input_vals = [values[k] for k in INPUT_KEYS]
 
-    # ensure we don't exceed limits
-    for value, limit, axis in zip(input_vals, AXES_LIMITS, INPUT_KEYS):
-        _value = float(value)
-        if _value < limit[0] or _value > limit[1]:
-            raise LimitsException(f"Axis {axis} value is out of range!")
-
-    conn.send(bytes("p" + 125 * " ", encoding="utf-8"))
-
-    out_str = "".join([str(s) + (9 - len(str(s))) * ' ' for s in input_vals])
-    out_bytes = bytes(out_str + (126-len(out_str)) * ' ', 'utf-8')
-    conn.send(out_bytes)
-    print('sent: ',out_bytes)
     register=values["PR_in"]
-    out_str = "".join(register)
-    out_bytes = bytes(out_str + (126-len(out_str)) * ' ', 'utf-8')
-    conn.send(out_bytes)
-    print('sent: ',out_bytes)
-    pass
+    print(f'Wartosc rejestru {register}')
+    if register.isnumeric():
+        # ensure we don't exceed limits
+        for value, limit, axis in zip(input_vals, AXES_LIMITS, INPUT_KEYS):
+            _value = float(value)
+            if _value < limit[0] or _value > limit[1]:
+                window['War'].update(f'Wartosc {axis} jest poza zakresem!')
+                raise LimitsException(f"Axis {axis} value is out of range!")
+        window['War'].update('')
+        conn.send(bytes("p" + 125 * " ", encoding="utf-8"))
+
+        out_str = "".join([str(s) + (9 - len(str(s))) * ' ' for s in input_vals])
+        out_bytes = bytes(out_str + (126-len(out_str)) * ' ', 'utf-8')
+        conn.send(out_bytes)
+        print('sent: ',out_bytes)
+        register=values["PR_in"]
+        out_str = "".join(register)
+        out_bytes = bytes(out_str + (126-len(out_str)) * ' ', 'utf-8')
+        conn.send(out_bytes)
+        print('sent: ',out_bytes)
+        pass
+    raise BadFormatException
 
 def quit(conn, **kwargs):
     conn.send(bytes("0" + 125 * " ", encoding="utf-8"))
@@ -48,12 +55,25 @@ def quit(conn, **kwargs):
     
 def dummy(conn, **kwargs):
     print("Method not implemented yet!")
-    pass
 
 def readpos(conn, **kwargs):
     conn.send(bytes("r" + 125 * " ", encoding="utf-8"))
     data = conn.recv(126)       # 126 bajtów, bo tyle przesyła Karel
-    print('received: ',data)
+    x,y,z,w,p,r = decode_coords(data)
+    window['X'].update(x)
+    window['Y'].update(y)
+    window['Z'].update(z)
+    window['W'].update(w)
+    window['P'].update(p)
+    window['R'].update(r)
+    pass
+
+def readpr(conn, **kwargs):
+    conn.send(bytes("e" + 125 * " ", encoding="utf-8"))
+    register=values["PR_read"]
+    out_str = "".join(register)
+    conn.send(bytes(out_str + (126-len(out_str)) * ' ', 'utf-8'))
+    data = conn.recv(126)       # 126 bajtów, bo tyle przesyła Karel
     x,y,z,w,p,r = decode_coords(data)
     window['X'].update(x)
     window['Y'].update(y)
@@ -73,8 +93,10 @@ def move(conn, **kwargs):
     data = conn.recv(126)       # 126 bajtów, bo tyle przesyła Karel
     if int(data) == 1:
         print('Pozycja osiagalna')
+        window['War'].update('')
     else:
         print('Nieosiagalna pozycja')
+        window['War'].update('Nieosiagalna pozycja!')
     pass
 
 def decode_coords(coords: bytes, separator: str=" "):
@@ -86,18 +108,18 @@ def decode_coords(coords: bytes, separator: str=" "):
     return tuple(decoded_coords)
 
 # FUNCTIONS USED TO INVOKE BEHAVIOUR AFTER BUTTON CLICK IN GUI
-event_functions = {"Write Pos": writepos, "Quit": quit, "Read CurPos": readpos, "Move":move}
+event_functions = {"Write Pos": writepos, "Quit": quit, "None":quit, "Read CurPos": readpos, "Read PR":readpr, "Move":move, "Dummy":dummy}
 
 
 # LAYOUT
 sg.theme('DarkAmber')
 layout = [  [sg.Text('Obecna pozycja robota')],
             [sg.Text('X'), sg.Text(size=(40,1), key='X')],
-            [sg.Text('Y'), sg.Text(size=(40,1), key='Y')],
+            [sg.Text('Y'), sg.Text(size=(40,1), key='Y'), sg.Button('Read CurPos')],
             [sg.Text('Z'), sg.Text(size=(40,1), key='Z')],
-            [sg.Text('W'), sg.Text(size=(40,1), key='W'),sg.Button('Read CurPos')],
-            [sg.Text('P'), sg.Text(size=(40,1), key='P')],
-            [sg.Text('R'), sg.Text(size=(40,1), key='R')],
+            [sg.Text('W'), sg.Text(size=(40,1), key='W')],
+            [sg.Text('P'), sg.Text(size=(40,1), key='P'), sg.Text('Wczytaj PR'), sg.Input(key="PR_read")],
+            [sg.Text('R'), sg.Text(size=(40,1), key='R'), sg.Button("Read PR"), sg.Text('(nie dziala na niezainicjowane')],
             [sg.Text('Docelowa pozycja')],
             [sg.Text('X'), sg.Input(key="X_in")],
             [sg.Text('Y'), sg.Input(key="Y_in")],
@@ -106,7 +128,8 @@ layout = [  [sg.Text('Obecna pozycja robota')],
             [sg.Text('P'), sg.Input(key='P_in')],
             [sg.Text('R'), sg.Input(key='R_in')],
             [sg.Text('Ruch Robota'), sg.Text('Cel PR'), sg.Input(key="PR_go"), sg.Button("Move")],
-            [sg.Button('Quit')] 
+            [sg.Button('Quit'), sg.Text('Warnings: '), sg.Text(size=(40,1), key='War')],
+            [sg.Button('Dummy')]
             ]
 
 # SERVER CONFIGURATION
@@ -155,11 +178,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 window['W_in'].update(w)
                 window['P_in'].update(p)
                 window['R_in'].update(r)
+
+                window['PR_in'].update('1')
+                window['PR_go'].update('1')
+                window['PR_read'].update('1')
                 first=False
             
             event, values = window.read()
 
             try:
+                # print(f'Event: {str(event)}') #wypisanie jaki event wystapil
                 event_functions[str(event)](conn, values=values)
             
             except QuitException as e:
@@ -167,6 +195,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             
             except LimitsException as e:
+                print(e)
+                conn.send(bytes(126 * ' ', encoding="utf-8"))
+            except BadFormatException as e:
                 print(e)
                 conn.send(bytes(126 * ' ', encoding="utf-8"))
 
