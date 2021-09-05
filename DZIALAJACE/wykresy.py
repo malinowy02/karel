879fa02e24a7accdd8a4b84a9156219c55966aca
Hubert Malinowski <hubert.malina@gmail.com>
2021-09-05 19:16:26 +0200

    Poprawki plotu wykresów matplot

--- DZIALAJACE/wykresy.py
+++ DZIALAJACE/wykresy.py
@@ -1,26 +1,40 @@
 import matplotlib.pyplot as m
-import plotly
+import plotly.graph_objects as go
+import numpy as np
+
+def dane(file):
+    data = []
+    for line in file.readlines():
+        tmp = line.split()
+        new = []
+        for str in tmp:
+            str.split('=')
+            new = new + str.strip(';').split('=')
+        data.append([new[3], new[5]])
+    x=[]
+    y=[]
+    for i in range(len(data)):
+        x.append(float(data[i][0]))
+        y.append(float(data[i][1]))
+    return x,y
+
 
 f = open('PomiarPC.txt', 'r')
-data = []
-for line in f.readlines():
-    tmp = line.split()
-    new = []
-    for str in tmp:
-        str.split('=')
-        new = new + str.strip(';').split('=')
-    data.append([new[3], new[5]])
-fd = open('Wyniki.txt', 'w')
+fm = open('PomiarMAC.txt', 'r')
 
-x=[]
-y=[]
-for i in range(len(data)):
-    x.append(float(data[i][0]))
-    y.append(float(data[i][1]))
+
+x,y = dane(f)
+xm,ym = dane (fm)
     
-m.plot(x,y, 'bo')
-m.plot(x,x)
+m.style.use('seaborn')
+m.plot(x, y, 'ko', label='Pomiary na jednym komputerze')
+m.plot(xm, ym, 'r^', label='Pomiary na dwóch komputerach')
+m.plot(x, x, label = 'Wartość teoretyczna')
 
+m.xlabel('Okres teoretyczny [ms]')
+m.ylabel('Okres rzeczywisty [ms]')
+m.legend()
 m.show()
+
 
 f.close()
